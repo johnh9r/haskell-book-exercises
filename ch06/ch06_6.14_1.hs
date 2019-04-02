6cc2fca45d702920aa6606725cafa503062bb235
Chris King-Parra <kingparra@users.noreply.github.com>
2019-04-02 21:35:27 +0000

    Add ascii art diagram to ch06_6.14_1.hs
    Add an ASCII art diagram breaking down where each element
    of the type signature of arith fits into its final implementation.

--- ch06/ch06_6.14_1.hs
+++ ch06/ch06_6.14_1.hs
@@ -1,2 +1,20 @@
 arith :: Num b => (a -> b) -> Integer -> a -> b
 arith f i a = (f a) + (fromInteger i)
+{-
+ - (+)    :: Num a =>   a      ->    a     ->     a
+ - (-)    :: Num a =>   a      ->    a     ->     a
+ - (*)    :: Num a =>   a      ->    a     ->     a
+ -                      ^       ^    ^      ^     ^
+ -                       \      |     \      \    |
+ -                        \     |      +---+  \   |
+ -                         \    |           \  \  |
+ -                          v   v            v  v v
+ - arith  :: Num b => (a -> b) -> Integer -> a -> b
+ -                     ^  ^          ^     ^
+ -                     |  |          |     |
+ -                     +  +          |     |
+ -                      \/           +-----------+
+ -                      /\                 |     |
+ -                     v  v                v     v
+ - arith f i a =      (f  a)    +   (fromInteger i)
+ -}
